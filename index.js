9928a90b94670aba45d1bfd09d1edae2d156c46d
github username <github email address>
2019-06-05 16:51:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// var descriptionInput = document.getElementsByTagName('INPUT')[1].value
+document.addEventListener('DOMContentLoaded', () => {
+  // your code here
+  const newTask = document.getElementById('new-task-description');
+  const submitButton = document.getElementsByTagName('INPUT')[1];
+  const list = document.getElementById('tasks');
+
+  submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const listElement = document.createElement('li');
+    consolelog(newTask.value)
+    listElement.innerHTML = newTask.value;
+    list.appendChild(listElement);
+  });
+});
